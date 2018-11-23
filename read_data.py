c3edd67f000f46d7ff8af1b089441bbedab30b78
Vikalp Kamdar <vikalp.kamdar@epfl.ch>
2018-11-23 21:59:42 +0100

    read 2012-2017

--- read_data.py
+++ read_data.py
@@ -11,7 +11,21 @@ spark.conf.set('spark.sql.session.timeZone', 'UTC')
 sc = spark.sparkContext
 
 sqlc = SQLContext(sc)
-df = sqlc.read.json('hdfs:///datasets/reddit_data/2017/RC_2017-01.bz2')
+#df = sqlc.read.json('hdfs:///datasets/reddit_data/2017/RC_2017-01.bz2')
+
+for i in range(2012,2017):
+    for j in range(1,13):
+        path = ""
+        save_path=""
+        if j<10:
+            path = 'hdfs:///datasets/reddit_data/' + str(i) + '/RC_' + str(i) + '-0' + str(j) + '.bz2'
+            save_path = str(i) + '_RC_' + str(i) + '-0' + str(j)
+        else:
+            path = 'hdfs:///datasets/reddit_data/' + str(i) + '/RC_' + str(i) + '-' + str(j) + '.bz2'
+            save_path = str(i) + '_RC_' + str(i) + '-' + str(j)
+        df = sqlc.read.json(path)
+        df.write.mode('overwrite').parquet('data/' + save_path + '.parquet')
+        print('Year: ' + str(i) + '     ' + 'month: ' + str(j) + ' saved.')
 
 #df.write.mode('overwrite').parquet("/home/difernan/data/sample2017.parquet")
-df.write.mode('overwrite').parquet("/home/kamdar/posts.parquet")
+#df.write.mode('overwrite').parquet("/home/kamdar/posts.parquet")
