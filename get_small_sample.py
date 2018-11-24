076990c69df6820da5bf0864deab0427ab79550b
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2018-11-24 23:49:32 +0100

    egnwdslnv

--- get_small_sample.py
+++ get_small_sample.py
@@ -25,7 +25,7 @@ path_dataset = hdfs + 'datasets/reddit_data/'
 # read data for January 2017 into a spark dataframe
 df = sqlc.read.json(path_dataset + '/2017/RC_2017-01.bz2')
 
-# sample 10% of the data
+# sample 1% of the data
 df_spark_slice = df_spark.sample(False,0.01)
 
 # create a parquet file with the sample on the user's hdfs
