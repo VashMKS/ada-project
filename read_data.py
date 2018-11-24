527562528879b45aa23424bab30bbad86e2670bb
Vikalp Kamdar <vikalp.kamdar@epfl.ch>
2018-11-24 18:21:04 +0100

    sample entire dataset

--- read_data.py
+++ read_data.py
@@ -13,19 +13,24 @@ sc = spark.sparkContext
 sqlc = SQLContext(sc)
 #df = sqlc.read.json('hdfs:///datasets/reddit_data/2017/RC_2017-01.bz2')
 
-for i in range(2012,2017):
-    for j in range(1,13):
-        path = ""
-        save_path=""
-        if j<10:
-            path = 'hdfs:///datasets/reddit_data/' + str(i) + '/RC_' + str(i) + '-0' + str(j) + '.bz2'
-            save_path = str(i) + '_RC_' + str(i) + '-0' + str(j)
-        else:
-            path = 'hdfs:///datasets/reddit_data/' + str(i) + '/RC_' + str(i) + '-' + str(j) + '.bz2'
-            save_path = str(i) + '_RC_' + str(i) + '-' + str(j)
-        df = sqlc.read.json(path)
-        df.write.mode('overwrite').parquet('data/' + save_path + '.parquet')
-        print('Year: ' + str(i) + '     ' + 'month: ' + str(j) + ' saved.')
+df = sqlc.read.parquet('hdfs:///user/difernan/data/*.parquet')#.sample(False,0.1)
+df = df.sample(False,0.1)
+df.write.mode('overwrite').parquet('/home/difernan/data/2012_2016_sampled.parquet')
+
+
+#for i in range(2012,2017):
+#    for j in range(1,13):
+#        path = ""
+#        #save_path=""
+#        if j<10:
+#            path = 'hdfs:///user/difernan/data/' + str(i) + '/RC_' + str(i) + '-0' + str(j) + '.bz2'
+#            save_path = str(i) + '_RC_' + str(i) + '-0' + str(j)
+#        else:
+#            path = 'hdfs:///user/difernan/data/' + str(i) + '/RC_' + str(i) + '-' + str(j) + '.bz2'
+#            save_path = str(i) + '_RC_' + str(i) + '-' + str(j)
+#        df = sqlc.read.parquet(path)
+        #df.write.mode('overwrite').parquet('data/' + save_path + '.parquet')
+        #print('Year: ' + str(i) + '     ' + 'month: ' + str(j) + ' saved.')
 
 #df.write.mode('overwrite').parquet("/home/difernan/data/sample2017.parquet")
 #df.write.mode('overwrite').parquet("/home/kamdar/posts.parquet")
