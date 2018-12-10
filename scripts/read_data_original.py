3e2efb556a109c427fbdebb58b6b5f40c6c8134c
Dídac Fernández Cadenas <d1dac_94@hotmail.com>
2018-12-10 01:02:12 +0100

    Update read_data_original.py

--- scripts/read_data_original.py
+++ scripts/read_data_original.py
@@ -15,21 +15,21 @@ sc = spark.sparkContext
 sqlc = SQLContext(sc)
 
 # features to keep
-to_keep = ['author', '', '', '', '', '', '', '']
+to_keep = ['author', 'subreddit_id', 'body', 'created_utc', 'score', 'controversiality', 'gilded', 'post_id', 'parent_id', 'link_id']
 
 # 2016
-df_2016 = sqlc.read.json('hdfs:///datasets/reddit_data/2017/RC_2016-*.bz2')
+df_2016 = sqlc.read.json('hdfs:///datasets/reddit_data/2016/RC_2016-*.bz2')
 
-df_2016_reduced = df.select(*to_keep)
+df_2016_reduced = df_2016.select(*to_keep)
 
-df_2016_reduced.write.mode('overwrite').parquet("full_reduced_dataet_2016.parquet")
+df_2016_reduced.write.mode('overwrite').parquet("full_dataset_2016.parquet")
 
 # 2017
 df_2017 = sqlc.read.json('hdfs:///datasets/reddit_data/2017/RC_2017-*.bz2')
 
 df_2017_reduced = df_2017.select(*to_keep)
 
-df_2017_reduced.write.mode('overwrite').parquet("full_reduced_dataset_2017.parquet")
+df_2017_reduced.write.mode('overwrite').parquet("full_dataset_2017.parquet")
 
 # union
 df_final = df_2016_reduced.union(df_2017_reduced)
